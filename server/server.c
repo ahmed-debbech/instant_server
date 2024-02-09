2ff53a2355f1b5b2ca1e8d2e3365e90f705bc3fa
ahmed-debbech <debbech.ahmed@gmail.com>
2024-02-09 16:07:28 +0100

    add http parsers and handlers for client request

--- server/server.c
+++ server/server.c
@@ -8,56 +8,117 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #include <unistd.h> // read(), write(), close()
-#define MAX 80 
-#define PORT 8080 
+#define MAX_BUFF_CLIENT 500
+#define MAX 900
+#define PORT 9888
+#define NUM_CNX 5
 #define SA struct sockaddr 
    
-// Function designed for chat between client and server. 
-void func(int connfd) {
+struct Sessions{
+    int arr[NUM_CNX];
+    int length;
+};
 
-    char buff[MAX]; 
-    int n; 
-    // infinite loop for chat 
-    for (;;) { 
-        bzero(buff, MAX); 
-   
-        // read the message from client and copy it in buffer 
-        read(connfd, buff, sizeof(buff)); 
-        // print buffer which contains the client contents 
-        printf("From client: %s\t To client : ", buff); 
-        bzero(buff, MAX); 
-        n = 0; 
-        // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') ; 
-   
-        // and send that buffer to client 
-        write(connfd, buff, sizeof(buff)); 
-   
-        // if msg contains "Exit" then server exit and chat ended. 
-        if (strncmp("exit", buff, 4) == 0) { 
-            printf("Server Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-   
-// Driver function 
-int main() 
-{ 
-    int sockfd, connfd, len; 
+struct Header{
+    char *key;
+    char *value;
+};
+
+struct HttpReq{
+
+    char *method;
+    char *path;
+
+    struct Header headers[50];
+    int len_header;
+
+    char *body;
+};
+
+void remove_spaces(char* s) {
+    char* d = s;
+    do {
+        while (*d == ' ') {
+            ++d;
+        }
+    } while (*s++ = *d++);
+}
+char **  get_parts_by_space(char * s){
+    char *token;
+    token = strtok(s, " ");
+    char ** devids;
+    int i = 0; 
+    while( token != NULL ) {
+        printf( "%s\n", token);
+        token = strtok(NULL, " ");
+        devids = realloc(devids, sizeof(char *));
+        devids[i] = token;
+        i++;
+    }
+    return devids;
+}
+
+struct HttpReq http_parser(char buff[]){
+    struct HttpReq request;
+
+    char *token;
+    token = strtok(buff, "\n");
+    //first line of raw http request parsing
+    if(strncmp("G", token,1) == 0){ //it is a GET req
+        strcpy(request.method, "GET");
+    }
+    if(strncmp("P", token,1) == 0){ // it is a POST req
+        strcpy(request.method, "POST");
+    }
+    get_parts_by_space(token);
+    token = strtok(buff, "\n");
+    while( token != NULL ) {
+        printf( "%s\n", token);
+        token = strtok(NULL, "\n");
+        
+    }
+    return request;
+}
+
+void handle(char buff[]){
+    printf("handle\n");
+    http_parser(buff);
+}
+
+void start_server(int sockfd){
+    
+    struct Sessions sess;
+    sess.length = 0;
+
+    while(1){
+        int connfd = accept(sockfd, 0,0); 
+        sess.arr[sess.length] = connfd;
+        sess.length++;
+
+        if (connfd < 0) { 
+            printf("server could not accept session with index %d\n", sess.length); 
+            return ;
+        }
+
+        char buff[MAX_BUFF_CLIENT];
+        read(connfd, buff, MAX_BUFF_CLIENT); 
+        handle(buff);
+    }
+}
+
+int main(){ 
+    int sockfd, connfd; 
     struct sockaddr_in servaddr, cli; 
-   
+    
     // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
         printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
+        return -1;
+    }
    
     // assign IP, PORT 
+    bzero(&servaddr, sizeof(servaddr)); 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     servaddr.sin_port = htons(PORT); 
@@ -65,32 +126,19 @@ int main()
     // Binding newly created socket to given IP and verification 
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
         printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-   
+        return -1; 
+    }
+
     // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
+    if ((listen(sockfd, NUM_CNX))  != 0) { 
         printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
-   
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
-        printf("server accept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server accept the client...\n"); 
-   
-    // Function for chatting between client and server 
-    func(connfd); 
-   
+        return -1; 
+    }
+
+    printf("Server listening on port %d\n", PORT);
+    start_server(sockfd);
+
     // After chatting close the socket 
     close(sockfd); 
+    return 0;
 }
