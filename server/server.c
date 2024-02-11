d8c84bf56be5031d656e8115367d5ba531578cf0
ahmed-debbech <debbech.ahmed@gmail.com>
2024-02-11 13:04:46 +0100

    successfully wrote an http request parser to struct

--- server/server.c
+++ server/server.c
@@ -8,9 +8,9 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #include <unistd.h> // read(), write(), close()
-#define MAX_BUFF_CLIENT 500
+#define MAX_BUFF_CLIENT 100000
 #define MAX 900
-#define PORT 9888
+//#define PORT 9888
 #define NUM_CNX 5
 #define SA struct sockaddr 
    
@@ -35,63 +35,130 @@ struct HttpReq{
     char *body;
 };
 
-void remove_spaces(char* s) {
-    char* d = s;
-    do {
-        while (*d == ' ') {
-            ++d;
+
+
+char ** get_token(char * buff, char tok, int * final_size){
+    //FILE *fptr;
+    //fptr = fopen("a.txt", "w");
+
+    buff[strlen(buff)] = tok;
+    int j, y=0;
+    int h = 0;
+    char **tokens = (char **) malloc(sizeof(char*) * (y+1));
+    char * m = malloc(sizeof(char) * (h+1)); 
+    for(j=0; j<=strlen(buff)-1; j++){
+        if((buff[j] != tok)){
+            m[h] = buff[j];
+            h++;
+            m = realloc(m, sizeof(char) * (h+1));
+        }else{
+            if(m != NULL){
+                h++;
+                m = realloc(m, sizeof(char) * (h+1));
+                m[h] = '\0';
+                tokens[y] = m;
+                //printf("D: %s\n", tokens[y]);
+                //fprintf(fptr, "%s", m);
+                y++;
+                tokens = (char **) realloc(tokens, sizeof(char*) * (y+1));
+                h = 0;
+                m = malloc(sizeof(char) * (h+1));
+            }
         }
-    } while (*s++ = *d++);
-}
-char **  get_parts_by_space(char * s){
-    char *token;
-    char * m;
-    strcpy(m, s);
-    token = strtok(m, " ");
-
-    size_t i = 1; 
-    char ** devids = NULL;
-    devids = malloc(sizeof(char **)*i);
-    strcpy(devids[0], token);
-    ///////////////////////////////////////////////////////////////// STOPEDD HERE
-    while( token != NULL ) {
-        printf("1\n");
-        token = strtok(NULL, " ");
-        printf("2\n");
-        devids = realloc(devids, sizeof(char *)*i);
-        printf("3\n");
-        strcpy(devids[0], token);
-        printf( "%s\n", devids[i-1]);
-        i++;
     }
-    return devids;
+    //fclose(fptr);
+    (*final_size) = (y);
+    return tokens;
 }
+int is_white_char(char c){
+    char * white = "\n \r\t";
+    for(int i=0;i<=strlen(white)-1; i++){
+        if(white[i] == c) return 1;
+    }
+    return 0;
+}
+char * trimer(char * s){
+    char * x = malloc(sizeof(char) * strlen(s));
+    int r=0;
+    for(int i=0; i<=strlen(s)-1; i++){
+        if(!is_white_char(s[i])){
+            x[r] = s[i];
+            r++;
+        }
+    }
+    return x;
+}
+struct HttpReq http_parser(char * buff){
 
-struct HttpReq http_parser(char buff[]){
+    printf("buffer: \n[%s]\n\n", buff);
     struct HttpReq request;
 
-    char *token;
-    token = strtok(buff, "\n");
-    //first line of raw http request parsing
-    if(strncmp("G", token,1) == 0){ //it is a GET req
-        strcpy(request.method, "GET");
-    }
-    if(strncmp("P", token,1) == 0){ // it is a POST req
-        strcpy(request.method, "POST");
-    }
-    get_parts_by_space(token);
-    token = strtok(buff, "\n");
-    while( token != NULL ) {
-        printf( "%s\n", token);
-        token = strtok(NULL, "\n");
+    int size;
+    char ** tokens = get_token(buff, '\n', &size);
+    int TOP_LINE = 0;
+    int BODY_START = size-1;
+    int counter = 0;
+    request.len_header = 0;
+    for(counter = 0; counter <= size-1; counter++){
+        
+        if(counter == TOP_LINE){ //first line of raw http request parsing
+            if(strncmp("G", tokens[counter],1) == 0){ //it is a GET req
+                request.method = (char *)malloc(3 * sizeof(char));
+                strcpy(request.method, "GET");
+            }
+            if(strncmp("P", tokens[counter],1) == 0){ // it is a POST req
+                request.method = (char *)malloc(4 * sizeof(char));
+                strcpy(request.method, "POST");
+            }
+            int s;
+            char ** parts = get_token(tokens[counter], ' ', &s);
+            request.path = malloc(sizeof(char) * 50);
+            strcpy(request.path, parts[1]);
+        }
+        if(counter == BODY_START){
+            request.body = malloc(sizeof(char) * strlen(tokens[counter]));
+            request.body = tokens[counter];
+        }
+        if((counter != TOP_LINE) && (counter != BODY_START)){ //it is a header
+            int s;
+            char ** parts = get_token(trimer(tokens[counter]), ':', &s);
+            if(s > 1 ){
+                request.headers[request.len_header].key = malloc(sizeof(char) * strlen(parts[0]));
+                if(s > 2){
+                    char * full_value = malloc(sizeof(char));
+                    for(int g=1; g<=s-1; g++){
+                        full_value = realloc(full_value, sizeof(char) * (strlen(parts[g])+1));
+                        strcat(full_value, parts[g]);
+                        if(g != s-1) //not the end yet!
+                        strcat(full_value, ":");
+                    }
+                    request.headers[request.len_header].value = malloc(sizeof(char) * strlen(full_value));
+                    request.headers[request.len_header].key = parts[0];
+                    request.headers[request.len_header].value = full_value;
+                }else{ //if it is 2
+                    request.headers[request.len_header].value = malloc(sizeof(char) * strlen(parts[1]));
+                    request.headers[request.len_header].key = parts[0];
+                    request.headers[request.len_header].value = parts[1];
+                }
+                request.len_header++;
+            }
+        }
         
     }
     return request;
 }
 
-void handle(char buff[]){
+void handle(char * buff){
     printf("handle\n");
-    http_parser(buff);
+    struct HttpReq req = http_parser(buff);
+    printf("%s %s %s %s %s %d\n",
+    req.path,
+    req.method,
+    req.headers[5].key,
+    req.headers[5].value,
+    req.body,
+    req.len_header
+    );
 }
 
 void start_server(int sockfd){
@@ -110,33 +177,43 @@ void start_server(int sockfd){
         }
 
         char buff[MAX_BUFF_CLIENT];
-        read(connfd, buff, MAX_BUFF_CLIENT); 
+        memset(buff,'\0', sizeof(buff));
+
+        ssize_t read_size = read(connfd, buff, MAX_BUFF_CLIENT); 
+        printf("read size: %ld\n", read_size);
         handle(buff);
     }
 }
-
+int * get_port(){
+    int sockfd;
+    struct sockaddr_in servaddr, cli; 
+    int PORT = 9879;
+    do{
+        PORT++;
+        // socket create and verification 
+        sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+        if (sockfd == -1) { 
+            printf("socket creation failed...\n"); 
+        }
+    
+        // assign IP, PORT 
+        bzero(&servaddr, sizeof(servaddr)); 
+        servaddr.sin_family = AF_INET; 
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+        servaddr.sin_port = htons(PORT); 
+    }while((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0);
+    int *arr = malloc(sizeof(int) * 2);
+    arr[0] = sockfd;
+    arr[1] = PORT;
+    return arr;
+}
 int main(){ 
     int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
-    
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        return -1;
-    }
-   
-    // assign IP, PORT 
-    bzero(&servaddr, sizeof(servaddr)); 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
-   
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        return -1; 
-    }
+    int PORT;
+
+    int * d = get_port();
+    sockfd = d[0];
+    PORT = d[1];
 
     // Now server is ready to listen and verification 
     if ((listen(sockfd, NUM_CNX))  != 0) { 
