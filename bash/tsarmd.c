74674d0026ed34fc8afdf5bc78b37166e5cb8ebc
ahmed-debbech <debbech.ahmed@gmail.com>
2024-03-14 14:48:54 +0100

    fix get req

--- bash/tsarmd.c
+++ bash/tsarmd.c
@@ -7,6 +7,8 @@
 #include <signal.h>
 #include <curl/curl.h>
 
+char * big_resp;
+
 char * read_msg(){
     FILE * file;
     file = fopen("/tmp/ms_bu", "r");
@@ -40,12 +42,17 @@ void send_srv(char * t){
 
 size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
     size_t realsize = size * nmemb;
-    printf("%s",(char *)contents);
+    //printf("%s",(char *)contents);
+    userp = (char*) malloc(sizeof(char) * 512);
+    big_resp = (char*) malloc(sizeof(char) * 512);
+    strcpy((char *)userp, (char *)contents);
+    strcpy((char *)big_resp, (char *)contents);
+    //printf("[%s]\n",(char *)userp);
     return realsize;
 }
 
 void make_get(){
-    printf("appel");
+    printf("appel\n");
 
     CURL *curl = curl_easy_init();
     CURLcode res;
@@ -54,6 +61,8 @@ void make_get(){
         const char *url = "http://localhost:9880";
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+
         CURLcode res = curl_easy_perform(curl);
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
@@ -62,14 +71,23 @@ void make_get(){
     }
 }
 
+void store(char * s){
+    char * current_msg = read_msg();
+    if(strcmp(current_msg, s) != 0){
+        printf("difff");
+        send_srv(s);
+    }
+    free(current_msg);
+}
+
 int main(int argc, char **argv){
 
-    int PORT = 9881;
+    int PORT = 988;
 
     while(1){
         make_get();
-        printf("lllll");
-       //sleep(1);
+        store(big_resp);
+        sleep(1);
     }
 
     return 0;
